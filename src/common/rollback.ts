0d4c34e8f4fe1a1dbc19c5494fccce88cb3c9da1
infloop <infloop.gm@gmail.com>
2022-05-03 15:45:42 +0300

    Working with piping #3

--- src/common/rollback.ts
+++ src/common/rollback.ts
@@ -1,5 +1,7 @@
 import { wrapError, wrapErrorTagged } from './wrapped-errors';
 
-export const makeRollback = <E extends Error, T, U extends [T, ...T[]]>(cause: E, actions: U) => ({ cause: wrapError(cause), actions});
+export const makeRollback = <E extends Error, T, U extends [T, ...T[]]>(cause: E, actions: U) => ({ cause: cause, actions});
+
+export const makeRollbackWrapped = <E extends Error, T, U extends [T, ...T[]]>(cause: E, actions: U) => ({ cause: wrapError(cause), actions});
 
 export const makeRollbackTagged = <E extends Error, T, U extends [T, ...T[]]>(cause: E, actions: U) => ({ cause: wrapErrorTagged(cause), actions});
